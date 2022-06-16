6e2920cc1961dd08d702ed2e64e48506e4458e59
su-de-sh <sudesh7443@gmail.com>
2022-06-16 15:07:38 +0545

    feat:horizontal and vertical match added

--- Second Phase/connect-four.js
+++ Second Phase/connect-four.js
@@ -15,7 +15,6 @@ function whoIsWinner(piecesPositionList) {
     myPositionMap.push(myMap.indexOf(element.split("_")[0]));
     myColorMap.push(element.split("_")[1]);
   });
-  console.log(myColorMap);
 
   for (let i = 0; i < myPositionMap.length; i++) {
     let currentRow = myGrid.length - 1;
@@ -28,21 +27,158 @@ function whoIsWinner(piecesPositionList) {
       myGrid[currentRow][myPositionMap[i]] = myColorMap[i];
       myGrid.length - 1;
     }
+
+    let matchHorizontal = horizontalCheck();
+    let matchVertical = verticalCheck();
+
+    if (matchHorizontal) {
+      console.log(myGrid);
+      console.log(matchHorizontal);
+      return matchHorizontal;
+    }
+    if (matchVertical) {
+      console.log(myGrid);
+      console.log(matchVertical);
+      return matchVertical;
+    }
   }
 
-  return myGrid;
+  // console.log(myGrid);
+  //CHECK FOR HORIZONTAL
+  function horizontalCheck() {
+    for (let i = 0; i < myGrid.length; i++) {
+      for (let j = 0; j < myGrid[i].length - 3; j++) {
+        let myElement = myGrid[i][j];
+        if (myElement) {
+          if (
+            myGrid[i][j] === myGrid[i][j + 1] &&
+            myGrid[i][j] === myGrid[i][j + 2] &&
+            myGrid[i][j] === myGrid[i][j + 3]
+          ) {
+            console.log("horizontal match");
+            console.log(i);
+            console.log(j + 3);
+            return myGrid[i][j];
+          }
+        }
+      }
+    }
+    return 0;
+  }
+  function verticalCheck() {
+    for (let i = 0; i < myGrid.length - 3; i++) {
+      for (let j = 0; j < myGrid[i].length; j++) {
+        let myElement = myGrid[i][j];
+        if (myElement) {
+          if (
+            myGrid[i][j] === myGrid[i + 1][j] &&
+            myGrid[i][j] === myGrid[i + 2][j] &&
+            myGrid[i][j] === myGrid[i + 3][j]
+          ) {
+            console.log("vertical match");
+            console.log(i + 3);
+            console.log(j);
+            return myGrid[i][j];
+          }
+        }
+      }
+    }
+    return 0;
+  }
+  // for (let i = myGrid.length - 1; i >= 0; i--) {
+  //   for (let j = 0; j < myGrid[i].length; j++) {
+  //     let myElement = myGrid[i][j];
+  //     if (myElement) {
+  //       if (
+  //         myGrid[i][j] === myGrid[i + 1][j] &&
+  //         myGrid[i][j] === myGrid[i + 2][j] &&
+  //         myGrid[i][j] === myGrid[i + 3][j]
+  //       ) {
+  //         console.log("vertical match");
+  //         return myGrid[i][j];
+  //       }
+  //     }
+  //   }
+  // }
+  // for (let i = myGrid.length - 1; i >= 0; i--) {
+  //   for (let j = 0; j < myGrid[i].length - 3; j++) {
+  //     let myElement = myGrid[i][j];
+
+  //     if (myElement) {
+  //       if (
+  //         myGrid[i][j] === myGrid[i + 1][j + 1] &&
+  //         myGrid[i][j] === myGrid[i + 2][j + 2] &&
+  //         myGrid[i][j] === myGrid[i + 3][j + 3]
+  //       ) {
+  //         console.log("diagonal match");
+  //         return myGrid[i][j];
+  //       }
+  //     }
+  //   }
+  // }
+  // for (let i = myGrid.length - 1; i >= 0; i--) {
+  //   for (let j = 0; j < myGrid[i].length - 3; j++) {
+  //     let myElement = myGrid[i][j];
+
+  //     if (myElement) {
+  //       if (
+  //         myGrid[i][j] === myGrid[i - 1][j + 1] &&
+  //         myGrid[i][j] === myGrid[i - 2][j + 2] &&
+  //         myGrid[i][j] === myGrid[i - 3][j + 3]
+  //       ) {
+  //         console.log("diagonal match");
+  //         return myGrid[i][j];
+  //       }
+  //     }
+  //   }
+  // }
+  // return "Draw";
 }
 
 // Driving code
 let array = [
-  "A_Red",
+  "C_Yellow",
+  "B_Red",
   "B_Yellow",
-  "A_Red",
-  "B_Yellow",
-  "A_Red",
-  "B_Yellow",
+  "E_Red",
+  "D_Yellow",
   "G_Red",
   "B_Yellow",
+  "G_Red",
+  "E_Yellow",
+  "A_Red",
+  "G_Yellow",
+  "C_Red",
+  "A_Yellow",
+  "A_Red",
+  "D_Yellow",
+  "B_Red",
+  "G_Yellow",
+  "A_Red",
+  "F_Yellow",
+  "B_Red",
+  "D_Yellow",
+  "A_Red",
+  "F_Yellow",
+  "F_Red",
+  "B_Yellow",
+  "F_Red",
+  "F_Yellow",
+  "G_Red",
+  "A_Yellow",
+  "F_Red",
+  "C_Yellow",
+  "C_Red",
+  "G_Yellow",
+  "C_Red",
+  "D_Yellow",
+  "D_Red",
+  "E_Yellow",
+  "D_Red",
+  "E_Yellow",
+  "C_Red",
+  "E_Yellow",
+  "E_Red",
 ];
 
-console.log(whoIsWinner(array));
+whoIsWinner(array);
