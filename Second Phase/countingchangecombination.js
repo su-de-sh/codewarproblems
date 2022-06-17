aff26524df963f18df087bb6c1f86e02bdc44adf
su-de-sh <sudesh7443@gmail.com>
2022-06-17 10:32:16 +0545

    counting change problem solved

--- Second Phase/countingchangecombination.js
+++ Second Phase/countingchangecombination.js
@@ -1,19 +1,23 @@
 var countChange = function (money, coins) {
   let myArr = [];
 
-  for (let i = 0; i < money; i++) {
+  for (let i = 0; i < money + 1; i++) {
     myArr.push(0);
   }
+  console.log(myArr);
 
   myArr[0] = 1;
+  console.log(myArr);
 
   for (let i = 0; i < coins.length; i++) {
     // console.log(myArr.length);
     for (let j = 1; j < myArr.length; j++) {
       if (j - coins[i] < 0) continue;
       myArr[j] += myArr[j - coins[i]];
+      //   console.log(myArr[j]);
     }
   }
-  return myArr[myArr.length - 1] + 1;
+  //   console.log(myArr);
+  return myArr[myArr.length - 1];
 };
-console.log(countChange(10, [5, 2, 3]));
+console.log(countChange(11, [5, 7]));
