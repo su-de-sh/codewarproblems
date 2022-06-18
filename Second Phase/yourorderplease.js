a9df648e899c738de824e524423ffa05cf6d7297
su-de-sh <sudesh7443@gmail.com>
2022-06-18 22:05:43 +0545

    problem pending to solve

--- Second Phase/yourorderplease.js
+++ Second Phase/yourorderplease.js
@@ -1,1 +1,6 @@
-function order(words) {}
+function order(words) {
+  console.log(words.split(" ").map());
+}
+//"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+
+order("is2 Thi1s T4est 3a"); //-->  "Thi1s is2 3a T4est")
