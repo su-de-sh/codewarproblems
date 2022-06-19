ccc3f10f93b14db42fa7499a512536258160c446
su-de-sh <sudesh7443@gmail.com>
2022-06-19 14:43:25 +0545

    about to solve

--- Second Phase/humanreadableduration.js
+++ Second Phase/humanreadableduration.js
@@ -1,14 +1,51 @@
 function formatDuration(seconds) {
-  let year = Math.floor(seconds / (3.154 * Math.pow(10, 7)));
-  let remSec = seconds % (3.154 * Math.pow(10, 7));
+  console.log(seconds);
+  if (!seconds) return "now";
+  let year = Math.floor(seconds / (3600 * 24 * 365)); //(3.154 * Math.pow(10, 7)
+  let remSec = seconds % (3600 * 24 * 365);
   let day = Math.floor(remSec / 86400);
   remSec = remSec % 86400;
   let hour = Math.floor(remSec / 3600);
   remSec = remSec % 3600;
   let minute = Math.floor(remSec / 60);
   let second = remSec % 60;
+  let myDuration = [year, day, hour, minute, second];
 
-  return `${year} ${day} ${hour} ${minute} ${second} `;
+  return convert(myDuration);
 }
 
-console.log(formatDuration(233662));
+function convert(myDuration) {
+  myDurationFormat = "";
+  let myDurationMap = ["year", "day", "hour", "minute", "second"];
+  myDuration.forEach((time, i) => {
+    if (time > 1) {
+      if (myDurationMap[i] === "second") {
+        myDurationFormat += `${time} ${myDurationMap[i]}s`;
+      } else {
+        if (myDuration[i + 1] && !myDuration[i + 2]) {
+          myDurationFormat += `${time} ${myDurationMap[i]}s and `;
+        } else if (myDuration[i + 1] && myDuration[i + 2]) {
+          myDurationFormat += `${time} ${myDurationMap[i]}s, `;
+        } else {
+          myDurationFormat += `${time} ${myDurationMap[i]}s`;
+        }
+      }
+    }
+    if (time === 1) {
+      if (myDurationMap[i] === "second") {
+        myDurationFormat += `${time} ${myDurationMap[i]} `;
+      } else {
+        if (myDuration[i + 1] && !myDuration[i + 2]) {
+          myDurationFormat += `${time} ${myDurationMap[i]} and `;
+        } else if (myDuration[i + 1] && myDuration[i + 2]) {
+          myDurationFormat += `${time} ${myDurationMap[i]}, `;
+        } else {
+          myDurationFormat += `${time} ${myDurationMap[i]}`;
+        }
+      }
+    }
+  });
+  return myDurationFormat.trim();
+}
+
+console.log(formatDuration(369121517));
